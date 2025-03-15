a21b11706d2b92c17d01631e00c46ea719acedbc
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-03-15 23:52:29 +0600

    dynamic video card section created

--- script/index.js
+++ script/index.js
@@ -1,10 +1,17 @@
 function loadCategories() {
-  //1 - fetching the data
+  //fetching the categories data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    //2 - converting promise "res" to json
+    //converting promise "res" to json
     .then((res) => res.json())
-    //3 - sending "data" to display
+    //sending "data" to display
     .then((data) => displayCategories(data.categories));
+}
+
+function loadVideos() {
+  // fetching the videos data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
 }
 
 function displayCategories(categories) {
@@ -22,4 +29,43 @@ function displayCategories(categories) {
   }
 }
 
+const displayVideos = (videos) => {
+  // get the container
+  const videoContainer = document.getElementById("videoContainer");
+  // loop operation on array of object
+  videos.forEach((video) => {
+    //create element
+    let videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+       <div class="card bg-base-100">
+          <figure class="relative">
+            <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Image" />
+            <span class="absolute bottom-2 right-2 bg-black text-white text-sm px-2 rounded">3hrs 56 min ago</span>
+          </figure>
+
+          <div class="flex gap-3 px-0 py-5">
+            <div class="profile">
+              <div class="avatar">
+                <div class="ring-primary ring-offset-base-100 w-7 rounded-full ring ring-offset-2">
+                  <img src="${video.authors[0].profile_picture}" />
+                </div>
+              </div>
+            </div>
+            <div class="intro">
+              <h2 class="text-sm font-semibold">${video.title}</h2>
+              <div class="flex gap-2">
+                <span class="text-sm text-gray-500">${video.authors[0].profile_name}</span>
+                <img class="w-5 h-5" src="./assets/icons8-verified-badge-48.png" alt="verified icon" />
+              </div>
+              <p class="text-sm text-gray-500">${video.others.views}</p>
+            </div>
+          </div>
+        </div>
+    `;
+    //append the element
+    videoContainer.append(videoCard);
+  });
+};
+
 loadCategories();
+loadVideos();
